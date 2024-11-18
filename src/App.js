fbc55610d9385f3aefed459e7eac4c42313fd483
김자반 <128600341+kimseaweed@users.noreply.github.com>
2024-11-18 23:18:54 +0900

    커밋

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 
 function App() {
 
   let post = "강남 우동 맛집";
-  let [글제목, b] = useState(['남자코트할인','강남우동맛집','파이썬독학']);
+  let [글제목, 글제목변경] = useState(['남자코트할인','강남우동맛집','파이썬독학']);
+  let [따봉, 따봉변경] = useState(0);
+
+  function 따봉업(){
+    따봉+=1
+    console.log(따봉);
+  }
 
   return (
     <div className="App">
@@ -13,7 +18,7 @@ function App() {
         <h4>블로그임</h4>
       </div>
       <div className="list">
-        <h4>{글제목[0]}</h4>
+        <h4>  <span onClick={()=>{글제목변경(글제목=['여자코트할인','강남우동맛집','파이썬독학'])}}> {글제목[0]}</span> <span onClick={ ()=>{따봉변경( 따봉+1)} }> ☞ </span> {따봉} </h4>
         <p>2월 17일 발행</p>
         <h4>{글제목[1]}</h4>
         <p>2월 17일 발행</p>
